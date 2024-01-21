a026179abbb48b88401584ed4cc20206ce7657e0
Ron Ziroby Romero <ziroby@ziroby.com>
2024-01-21 22:13:43 +0000

    Add med list

---
+++ src/MedList.js
@@ -0,0 +1,10 @@
+import "./App.css"
+
+export default function MedList() {
+    return <div class="medlist">
+        ○ Early morning<br/>
+        ○ Lunch<br/>
+        ○ Post-Lunch<br/>
+        ○ Evening<br/>
+    </div>
+}
--- src/Panel.js
+++ src/Panel.js
@@ -1,10 +1,15 @@
+import MedList from "./MedList";
 import TaskList from "./TaskList";
 
 export default function Panel({ width, height }) {
     return <div class="panel" style={{ minWidth: width }}>
+        <h1>Sat Dec 1</h1>
         <div class="tasklistrow">
-            <TaskList width={width / 2.5} height={height / 4} title="Home" />
-            <TaskList width={width / 2.5} height={height / 4} title="Work" />
+            <TaskList width={width / 2.5} height={height / 2} title="Home" />
+            <TaskList width={width / 2.5} height={height / 2} title="Work" />
+        </div>
+        <div class="tasklistrow">
+             <MedList/>
         </div>
     </div>
 }
